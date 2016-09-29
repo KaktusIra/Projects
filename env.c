deeea0d0d25480157adbf7d3a211327272a8dcb4
KaktusIra <malikova.ie@phystech.edu>
2016-09-29 17:30:55 +0900

    Переменные окружения

---
+++ env.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+extern char ** environ;
+
+int main ()
+{
+int i;
+char *user = "ira";
+for (i = 0; i < 1000; i++)
+{
+	if ((strstr(environ[i], user)) != NULL)
+	{
+		printf("%s\n", environ[i]);
+	}
+}
+return 0;
+}
+
+   
