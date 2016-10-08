3282be2581e33c056aea8275dbe440505cb3ded2
KaktusIra <malikova.ie@phystech.edu>
2016-10-08 20:34:12 +0900

    Процессы (fork())
    :)

--- part3_task2 (2).c
+++ part3_task2 (2).c
@@ -6,14 +6,16 @@ pid_t pid;
 int main(void){
         int i, n, parrent, m,P;
         scanf("%d", &n);
-        parrent=getpid();
-        for (i=0; i<n; ++n){
-            pid = fork();
-            m=getppid();
+        parrent=m=getpid();
+        printf("My PID is %d\n",m);
+        for (i=0; i<(n/2); ++i){
             if (m==parrent){
+                pid = fork();
+                m=getppid();
                 P=getpid();
                 printf("My PID is %d\n",P);
 	        printf("End of child's process\n");
+                exit(1);
             };
         };
 } 
