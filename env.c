f37b00870a19dd91d5f6bed01c4859c73701e049
KaktusIra <malikova.ie@phystech.edu>
2016-10-19 18:10:08 +0900

    Исправления

--- env.c
+++ env.c
@@ -1,22 +1,35 @@
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <unistd.h>
+
 #include <string.h>
+
 
 extern char ** environ;
 
-int main ()
+
+int main (){
+
+    int i;
+
+    char *user = "ira";
+
+    for (i = 0; i < 1000; i++)
 {
-int i;
-char *user = "ira";
-for (i = 0; i < 1000; i++)
-{
-	if ((strstr(environ[i], user)) != NULL)
+	
+        if ((strstr(environ[i], user)) != NULL)
 	{
-		printf("%s\n", environ[i]);
-	}
-}
-return 0;
+		
+        printf("%s\n", environ[i]);
+	
+        }
+
+    }
+
+    return 0;
+
 }
 
    
